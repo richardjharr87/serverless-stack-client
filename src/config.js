7d45c2a4c9d69288d50571280cc633f9d218f13d
richardjharr87 <66270343+richardjharr87@users.noreply.github.com>
2020-07-08 17:06:57 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "richard-harrington-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://rfgocx61dl.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_8QBn2HKeN",
+      APP_CLIENT_ID: "1d4gj9cii716250nrsasd649ve",
+      IDENTITY_POOL_ID: "us-east-1:549e51fb-2a9f-428b-a295-9b17356e32f5"
+    }
+  };
